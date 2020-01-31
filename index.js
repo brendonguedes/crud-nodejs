9aadb77ec0d16ef324fbc0997a09bbcc9ebdcfc7
brendonguedes <lordbguedes@gmail.com>
2020-01-31 19:14:01 -0300

    comentários mais alterações GeekHunter

--- index.js
+++ index.js
@@ -2,78 +2,75 @@ const express = require('express');
 
 const server = express();
 
-server.use(express.json()); 
+server.use(express.json()); // faz com que o express entenda JSON
 
 // Query params = ?teste=1
-// Route params = /users/1
+// Route params = /geeks/1
 // Request body = { "name": "Brendon", "email": "brendon@email.com"}
 
 // CRUD - Create, Read, Update, Delete
 
-const users = ['Brendon', 'Lara', 'Elijah', 'Gregory'];
+const geeks = ['Brendon', 'Lara', 'Gregory', 'Hunter'];
 
-server.use((req, res, next) => {
-  console.time('Request');
-  console.log(`Método: ${req.method}; URL: ${req.url}; `);
+server.use((req, res, next) => { // server.use cria o middleware global
+  console.time('Request'); // marca o início da requisição
+  console.log(`Método: ${req.method}; URL: ${req.url}; `); // retorna qual o método e url foi chamada
 
-  next();
+  next(); // função que chama as próximas ações 
 
-  console.log('Finalizou');
+  console.log('Finalizou'); // será chamado após a requisição ser concluída
 
-  console.timeEnd('Request');
+  console.timeEnd('Request'); // marca o fim da requisição
 });
 
-function checkUserExists(req, res, next) {
+function checkGeekExists(req, res, next) {
   if (!req.body.name) {
-    return res.status(400).json({ error: 'User name is required' });
-
+    return res.status(400).json({ error: 'geek name is required' });
+    // middleware local que irá checar se a propriedade name foi infomada, 
+    // caso negativo, irá retornar um erro 400 - BAD REQUEST 
   }
-  return next();
-}
-
-function checkUserInArray(req, res, next) {
-  const user = users[req.params.index];
-  if (!user) {
-    return res.status(400).json({ error: 'User does not exists' });
-  }
-  req.user = user;
-
-  return next();
-}
-
-server.get('/users', (req, res) => {
-  return res.json(users);
-})
-
-server.get('/users/:index', checkUserInArray, (req, res) => {
-  return res.json(req.user);
-})
-
-server.post('/users', checkUserExists, (req, res) => {
-  const { name } = req.body;
+  return next(); // se o nome for informado corretamente, a função next() chama as próximas ações
+} 
   
-  users.push(name);  
+function checkGeekInArray(req, res, next) {
+  const geek = geeks[req.params.index];
+  if (!geek) {
+    return res.status(400).json({ error: 'geek does not exists' });
+  } // checa se o Geek existe no array, caso negativo informa que o index não existe no array
 
-  return res.json(users);
+  req.geek = geek;
 
+  return next();
+}
+
+server.get('/geeks', (req, res) => {
+  return res.json(geeks);
+}) // rota para listar todos os geeks
+
+server.get('/geeks/:index', checkGeekInArray, (req, res) => {
+  return res.json(req.geek);
 })
 
-server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
-  const { index } = req.params;
+server.post('/geeks', checkGeekExists, (req, res) => {
+  const { name } = req.body; // assim esperamos buscar o name informado dentro do body da requisição  
+  geeks.push(name);
+  return res.json(geeks); // retorna a informação da variavel geeks
+})
+
+server.put('/geeks/:index', checkGeekInArray, checkGeekExists, (req, res) => {
+  const { index } = req.params; // recupera o index com os dados
   const { name } = req.body;
+  geeks[index] = name; // sobrepõe/edita o index obtido na rota de acordo com o novo valor
+  return res.json(geeks);
+}); // retorna novamente os geeks atualizados após o update
 
-  users[index] = name;
+server.delete('/geeks/:index', checkGeekInArray, (req, res) => {
+  const { index } = req.params; // recupera o index com os dados
 
-  return res.json(users);
-});
-
-server.delete('/users/:index', checkUserInArray, (req, res) => {
-  const { index } = req.params;
-
-  users.splice(index, 1);
+  geeks.splice(index, 1); // percorre o vetor até o index selecionado e deleta uma posição no array
 
   return res.send();
-});
+}); // retorna os dados após exclusão
 
 
 server.listen(3000);
